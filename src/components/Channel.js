d8dd6e85bf6e596a5c2db9474ceabd1e9399a44f
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-23 21:43:28 -0400

    working on layout of landing page

---
+++ src/components/Channel.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import matches from '../assets/matches.png';
+import '../components/Channel.css'
+
+
+
+export default class Channel extends Component {
+render(){
+  return (
+    <div className= "Channel">
+
+
+    </div>
+  );
+}
+}
+
