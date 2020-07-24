c2e0976ec9b8d046abd5d94482c364ab0d852144
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-23 21:46:44 -0400

    adding the words that will display on the landing page

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -10,6 +10,11 @@ render(){
     <div className= "Channel">
       <div className = "landing-map">
         <img src={matches} alt = "matches" />
+        <div className = "Landing-page">
+          <h2>Issue ???</h2>
+          <h1>Catalysts</h1>
+          <h3>Agents of change</h3>
+          <h3>Read more</h3>
 
 
     </div>
